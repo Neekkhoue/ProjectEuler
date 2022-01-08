6e61b92938ab91cd6a6c29a671558ed728e401bf
NicoSantamaria <nsanta1101@outlook.com>
2022-01-08 17:44:38 -0500

    Update 1.py
    Revision

--- Solutions/1.py
+++ Solutions/1.py
@@ -1,12 +1,9 @@
 # Find the sum of all multiples of 3 or 5 below 1000
-
-numbers = list(range(1,1000))
 _sum = 0
 
-for x in numbers:
-    if not x % 5:
-        _sum += x
-    if (not x % 3) and x % 5:
+for x in list(range(1,1000)):
+    if (not x % 5) or (not x % 3):
         _sum += x
 
 print(_sum)
+# 233168
