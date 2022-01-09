592d58144e48ca73f2d317327d060f598341b54f
NicoSantamaria <nsanta1101@outlook.com>
2022-01-09 15:39:06 -0500

    Update 4.py
    Revised

--- Solutions/4.py
+++ Solutions/4.py
@@ -1,16 +1,10 @@
 # Find the largest palindrome that is the product of two 3-digit numbers
-factor1 = list(range(100,1000))
-factor2 = list(range(100,1000))
 palindrome = 0
 
-def isPalindrome(number):
-    return str(number) == str(number)[::-1]
-
-for x in factor1:
-    for y in factor2:
-        product = x * y
-        if isPalindrome(product) and product > palindrome:
-            palindrome = product
+for x in range(999, 100, -1):
+    for y in range(999, 100, -1):
+        if str(x * y) == str(x * y)[::-1] and x * y > palindrome:
+            palindrome = x * y
 
 print(palindrome)
 # 906609
